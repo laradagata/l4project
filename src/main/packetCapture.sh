a801b749d2c9b8b647ebc4057d017d7377814e4c
laradagata <laradagata@ubuntuvirtual2.myguest.virtualbox.org>
2023-12-12 13:01:27 +0000

    Obtained qlog files with quiche

--- src/main/packetCapture.sh
+++ src/main/packetCapture.sh
@@ -20,61 +20,63 @@ done < "$top_urls"
 output_dir_quic="/home/laradagata/l4project/data/PacketCaptures_test/testdata/quic"
 output_dir_tcp="/home/laradagata/l4project/data/PacketCaptures_test/testdata/tcp"
 
-#mkdir -p "$output_dir"
-
-#sudo docker run --rm -it ymuski/curl-http3 whoami
-
+# Iterate over websited and gather only QUIC-related information
 for website in "${websites[@]}"; do
+
 	# Extract the domain to use as the filename
 	filename=( $(awk -F'/' '{print $3}' <<< "$website"))
-	
 	echo "$website"
+	
+	# Make new directory to store files relating to this webiste
+	mkdir -p "$output_dir_quic/$filename"
+	
+	# Set QLOGDIR
+	export QLOGDIR="$output_dir_quic/$filename"
 	
 	# Start tcpdump to capture QUIC network traffic 
 	# Backgrounding this block to establish two threads
 	{
-		sudo tcpdump -n udp -SX -i any port 443 -w "$output_dir_quic/$filename.pcap"
+		sudo tcpdump -n udp -SX -i any port 443 -w "$output_dir_quic/$filename/$filename-tcpdump.pcap"
 	} &
 
 	# allow backgrounded block to start running before curl command
 	sleep 1
 	
-	#export QLOGDIR="$output_dir" 
-	
 	# Use curl to fetch the web page
-	#sudo docker run -it ymuski/curl-http3 export QLOGDIR=/opt
-	sudo docker run -it --rm ymuski/curl-http3 /bin/bash -c "export QLOGDIR=/opt && curl --http3 -IL "$website" && find -type f -name '*.sqlog' | xargs cat " > "$output_dir_quic/$filename.html"
-	#sudo docker run -it --rm ymuski/curl-http3 curl -V
+	sudo docker run -it --rm ymuski/curl-http3 curl --http3 -IL "$website" > "$output_dir_quic/$filename/$filename-curl.html"
+	
+	# Get qlog information for the web page
+	cd $HOME/quiche && cargo run --bin quiche-client -- "$website"
+	
+	# Kill tcpdump
 	kill -HUP $1
 	
 done
 
+# Iterate over websited and gather only TCP-related information
 for website in "${websites[@]}"; do
-# Extract the domain to use as the filename
+
+	# Extract the domain to use as the filename
 	filename=( $(awk -F'/' '{print $3}' <<< "$website"))
-	
 	echo "$website"
 	
-	# Start tcpdump to capture QUIC network traffic 
+	# Make new directory to store files relating to this webiste
+	mkdir -p "$output_dir_tcp/$filename"
+	
+	# Start tcpdump to capture TCP network traffic 
 	# Backgrounding this block to establish two threads
 	{
-		sudo tcpdump -i enp0s3 tcp -w "$output_dir_tcp/$filename.pcap"
+		sudo tcpdump -i enp0s3 tcp -w "$output_dir_tcp/$filename/$filename-tcpdump.pcap"
 	} &
 
 	# allow backgrounded block to start running before curl command
 	sleep 1
 	
-	#export QLOGDIR="$output_dir" 
-	
 	# Use curl to fetch the web page
-	#sudo docker run -it ymuski/curl-http3 export QLOGDIR=/opt
-	sudo curl -D "$output_dir_tcp/$filename.html" -vs "$website"
-	#sudo docker run -it --rm ymuski/curl-http3 curl -V
+	sudo curl -D "$output_dir_tcp/$filename/$filename-curl.html" -vs "$website"
+	
+	# Kill tcpdump
 	kill -HUP $1
 done
-
-#sudo docker run -it ymuski/curl-http3 ls /opt
-	
-# Stop tcpdump
 
 echo "Packet Capture Complete"
