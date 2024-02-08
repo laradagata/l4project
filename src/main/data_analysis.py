f06f142eb33d8f42a862327bdc767db69566ec2f
Lara D'Agata <2526633d@student.gla.ac.uk>
2024-02-08 21:42:40 +0000

    Obtained average handshake time for qlog files

--- src/main/data_analysis.py
+++ src/main/data_analysis.py
@@ -2,6 +2,7 @@ import os
 import json
 import pprint
 import pandas as pd
+from IPython.display import display
 
 
 def parseQlogFile(qlog_file_path):
@@ -10,36 +11,28 @@ def parseQlogFile(qlog_file_path):
 
     records = qlog_data.strip().split(regex)
     parsedRecords = parseQlogDataRecords(records)
-    data_dict = createDictionary(parsedRecords)
+    #data_dict = createDictionary(parsedRecords)
+    pprint.pprint(parsedRecords[0])
+
+    #df = pd.MultiIndex(levels=[['zero', 'one'], ['x','y']], labels=[[1,1,0,],[1,0,1,]])
 
     #df = pd.DataFrame(data_dict)
     #df = pd.concat({k: pd.DataFrame(v).T for k, v in data_dict.items()}, axis=0)
-    flat_dict = flatten_dict(data_dict)
 
     #df = pd.DataFrame.from_dict(flat_dict, orient="index")
     #df.index = pd.MultiIndex.from_tuples(df.index)
     #df = df.unstack(level=-1)
     #df.columns = df.columns.map("{0[1]}".format)
 
-    df = pd.DataFrame.from_dict(data_dict, orient='index')
-    #df = pd.concat({k: pd.DataFrame(v).T for k, v in data_dict.items()}, axis=0)
+    df = pd.DataFrame.from_dict(parsedRecords[0], orient='index')
 
-    print(df.T)
+
+    #df = pd.concat({k: pd.DataFrame(v).T for k, v in data_dict.items()}, axis=0)
+    #fr = pd.MultiIndex.from_frame(df)
+
+    display(df)
 
     f.close()
-
-def flatten_dict(nested_dict):
-    res = {}
-    if isinstance(nested_dict, dict):
-        for k in nested_dict:
-            flattened_dict = flatten_dict(nested_dict[k])
-            for key, val in flattened_dict.items():
-                key = list(key)
-                key.insert(0, k)
-                res[tuple(key)] = val
-    else:
-        res[()] = nested_dict
-    return res
   
 def parseQlogDataRecords(records):
     #records = [dict(i) for i in records if len(i) > 1]
@@ -48,15 +41,22 @@ def parseQlogDataRecords(records):
     #pprint.pprint(parsed_data)
     return parsed_data
 
-def createDictionary(parsed_data, data_dict={}):
-    #data_dict = {}
+def createDictionary(parsed_data):
+    data_dict = {}
 
     for data in parsed_data:
-        for key, value in data.items():
-            if key not in data_dict:
-                data_dict[key] = [value]
-            else:
-                data_dict[key].append(value)
+        if type(data) == dict:
+            for key, value in data.items():
+                if key not in data_dict:
+                    data_dict[key] = [value]
+                else:
+                    if type(value) == dict:
+                        data_dict[key].append(createDictionary(value))
+                    else:
+                        data_dict[key].append(value)
+        else:
+            return parsed_data
+
     
     #pprint.pprint(data_dict)
     return data_dict
@@ -68,20 +68,24 @@ path = "C:/Users/larad/Documents/l4project/data/packetCapture_home/quic"
   
 # Change the directory 
 os.chdir(path) 
-
+ctr = False
+#qlog_files=0
 # iterate through all file 
 for folder in os.listdir(): 
     #print("inside " + folder)
     quic_folder_path = f"{path}/{folder}"
     os.chdir(quic_folder_path)
 
-    for file in os.listdir():
-        #print("inside "+ file)
-        # Check whether file is in sqlog format or not 
-        if file.endswith(".sqlog"): 
-            parseQlogFile(f"{quic_folder_path}/{file}")
-            #print("file "+qlog_file_path+" has been opened")
-            break
-        break
+    if ctr == False:
+        for file in os.listdir():
+            #print("inside for")
+            # Check whether file is in sqlog format or not 
+            if file.endswith(".sqlog"): 
+                parseQlogFile(f"{quic_folder_path}/{file}")
+                #print("file "+qlog_file_path+" has been opened")
+                ctr=True
+                #qlog_files += 1
+
     
+#print(qlog_files)    
     
