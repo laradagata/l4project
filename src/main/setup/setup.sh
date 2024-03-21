6eb1aa94af44be318da25ff36f744681599bc210
Lara D'Agata <2526633d@student.gla.ac.uk>
2024-03-21 01:12:29 +0000

    Moved setup files to src/main/setup/

---
+++ src/main/setup/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install setup and dependencies 
+sudo apt install tcpdump
+sudo apt install curl
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install wireshark
+
+sudo apt update
+sudo apt -y install gcc
+sudo apt -y install cmake
+sudo apt -y install g++
+sudo apt install cargo
+
+# Install rust compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# Download Quiche project
+cd $HOME && git clone --recursive https://github.com/cloudflare/quiche 
+
+# Build Quiche
+cd $HOME/quiche && cargo build --examples
+
+# Verify installation
+cd $HOME/quiche && cargo test
